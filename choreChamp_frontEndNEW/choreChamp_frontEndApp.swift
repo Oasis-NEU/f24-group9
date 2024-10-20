d0fb14961d6279d42632c9b167e2e6d99a5b3e32
Jayden Ngo <134519694+23jango@users.noreply.github.com>
2024-10-20 12:40:38 -0400

    Add files via upload
    login screen, home screen, sign up screen

---
+++ choreChamp_frontEndNEW/choreChamp_frontEndApp.swift
@@ -0,0 +1,17 @@
+//
+//  choreChamp_frontEndApp.swift
+//  choreChamp_frontEnd
+//
+//  Created by Jayden Ngo on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct choreChamp_frontEndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
